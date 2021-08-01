6dee3d8e906c9fc11dff91395578263f444248ed
Valerii Bezuglyi <valnocorner@gmail.com>
2021-08-01 14:15:38 +0300

    initial

---
+++ Feed/Feed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Feed
+//
+//  Created by Valeriy Bezuglyy on 01.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
