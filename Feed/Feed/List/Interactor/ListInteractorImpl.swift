c23050c8a859dbdbfb0951495ee35833361e2e83
Valerii Bezuglyi <valnocorner@gmail.com>
2021-08-16 21:59:11 +0300

    take data from repo

--- Feed/Feed/List/Interactor/ListInteractorImpl.swift
+++ Feed/Feed/List/Interactor/ListInteractorImpl.swift
@@ -8,17 +8,14 @@
 import Foundation
 
 class ListInteractorImpl {
-    
+    let repo = FeedItemsRepo()
 }
 
 extension ListInteractorImpl: ListInteractor {
     func loadItems(_ completion: @escaping ([FeedItem]) -> Void) {
-        DispatchQueue.main.asyncAfter(deadline: .now() + 0.25) {
-            completion([FeedItem(title: "",
-                                 author: "",
-                                 date: Date(),
-                                 text: "",
-                                 imageUrl: "")])
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.25) { [weak self] in
+            guard let __self = self else { return }
+            completion(__self.repo.items())
         }
     }
 }
